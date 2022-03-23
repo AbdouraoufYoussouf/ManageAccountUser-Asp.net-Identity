0da377141e5934b71ec52d96a8e4687de80a7bd9
Abdouraouf <youssouf.abdouraouf.4@gmail.com>
2022-03-24 00:06:17 +0100

    Ajoutez des fichiers projet.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using LoginRegisterUser.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginRegisterUser.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>().ToTable("Users", "security"); //il prend en parametre l'entité et le shema de la BD
+            builder.Entity<IdentityRole>().ToTable("Roles", "security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RolesClaims", "security");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "security");
+        }
+    }
+}
